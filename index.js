eeefe376fbe24d22222aab5ee444c37814e760de
ALVIAN UXIO Inc. <105962746+alvianxxuxio@users.noreply.github.com>
2024-10-16 07:28:45 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -985,54 +985,38 @@ async function gptlogic(inputText, customPrompt) {
 }
 
 //gemini
-async function gemini(inputText) {
+async function gemini(options) {
   try {
-    const safetySettings = [
-      { category: HarmCategory.HARM_CATEGORY_HATE_SPEECH, threshold: HarmBlockThreshold.BLOCK_NONE },
-      { category: HarmCategory.HARM_CATEGORY_HARASSMENT, threshold: HarmBlockThreshold.BLOCK_NONE },
-      { category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT, threshold: HarmBlockThreshold.BLOCK_NONE },
-      { category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT, threshold: HarmBlockThreshold.BLOCK_NONE },
-    ];
-
-    const apiKey = 'AIzaSyD7ciBCgOP2DLXfpUDn-XrvoZnoUe0vZKc';
-    const genAI = new GoogleGenerativeAI(apiKey);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", safetySettings });
-
-    const generationConfig = {
-      temperature: 1,
-      topP: 0.95,
-      topK: 64,
-      maxOutputTokens: 8192,
-      responseMimeType: "text/plain",
-    };
-
-    const currentTime = getCurrentTimeInJakarta();
-    const todayDate = getTodayDate();
-
-    const chatSession = await model.startChat({
-      generationConfig,
-      history: [
-        {
-          role: 'user',
-          parts: [
-            {
-              text: `kamu adalah gemini, ai yang di buat oleh google, Kamu dapat mengetahui jam dan sekarang adalah hari ${currentTime} dan ${todayDate}.`,
-            },
-          ],
-        },
-        {
-          role: 'model',
-          parts: [
-            { text: 'Oke' },
-          ],
-        },
-      ],
-    });
-
-    const result = await chatSession.sendMessage(inputText);
-    return result.response.text();
-  } catch (error) {
-    console.error("Error in Renvy function:", error);
+    return await new Promise(async(resolve, reject) => {
+      options = {
+        model: "gemini-pro",
+        messages: options?.messages,
+        temperature: options?.temperature || 0.9,
+        top_p: options?.top_p || 0.7,
+        top_k: options?.top_p || 40,
+      }
+      if(!options?.messages) return reject("missing messages input payload!");
+      if(!Array.isArray(options?.messages)) return reject("invalid array in messages input payload!");
+      if(isNaN(options?.top_p)) return reject("invalid number in top_p payload!");
+      if(isNaN(options?.top_k)) return reject("invalid number in top_k payload!");
+      axios.post("https://api.acloudapp.com/v1/completions", options, {
+        headers: {
+          authorization: "sk-9jL26pavtzAHk9mdF0A5AeAfFcE1480b9b06737d9eC62c1e"
+        }
+      }).then(res => {
+        const data = res.data;
+        if(!data.choices[0].message.content) return reject("failed get response message!")
+        resolve({
+          success: true,
+          answer: data.choices[0].message.content
+        })
+      }).catch(reject)
+    })
+  } catch (e) {
+    return {
+      success: false,
+      errors: [e]
+    }
   }
 }
 
